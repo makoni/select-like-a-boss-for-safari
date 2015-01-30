8a8ebe5c2d1660d6e6c5fda3ca18d8b49731fa85
unknown <isys_bratukha@isys-win250.isys.uni-stuttgart.de>
2015-01-30 11:09:34 +0100

    don't extend window object and clean up

--- end.js
+++ end.js
@@ -21,31 +21,26 @@
 	THE SOFTWARE.
 
 \**********************************************************************************/
-window.SelectLikeABoss = window.SelectLikeABoss || (function() {
-	var getCurrentAnchor = function(node) {
-		if (node.nodeType === 3)
-			node = node.parentNode;
-		do {
-			if (node.constructor === HTMLAnchorElement)
-				return node
-		} while ((node = node.parentNode) && node !== document.body);
-		return null
+(function() {
+	'use strict';
+
+	function getCurrentAnchor(node) {
+		if		(node.href)			return node;
+		else if (!node.parentNode)	return null;
+		else						return getCurrentAnchor( node.parentNode );
 	};
 	var stopEvent = function(e) {
-		return e.preventDefault(), e.stopPropagation(), false
+		return e.preventDefault(), e.stopPropagation(), false;
 	};
 	var browser = (function() {
 		var w = typeof InstallTrigger === 'undefined';
 		return {getRangeFromPoint: function(x, y) {
-				if (w) {
-					return document.caretRangeFromPoint(x, y)
-				} else {
-					var range = document.createRange();
-					var p = document.caretPositionFromPoint(x, y);
-					range.setStart(p.offsetNode, p.offset);
-					return range
-				}
-				return null
+				if (w) return document.caretRangeFromPoint(x, y);
+				
+				var range = document.createRange();
+				var p = document.caretPositionFromPoint(x, y);
+				range.setStart(p.offsetNode, p.offset);
+				return range;
 			},userSelect: ((w ? '-webkit-' : '-moz-') + 'user-select')}
 	})();
 	var _letUserSelect = (function() {
@@ -57,29 +52,26 @@ window.SelectLikeABoss = window.SelectLikeABoss || (function() {
 				n = node, s = [];
 				for (var i = o.length - 1; i >= 0; i -= 1) {
 					s.push([n.style.getPropertyValue(o[i].p), n.style.getPropertyPriority(o[i].p)]);
-					n.style.setProperty(o[i].p, o[i].v, 'important')
+					n.style.setProperty(o[i].p, o[i].v, 'important');
 				}
 			} else if (n) {
 				for (var i = o.length; i-- > 0; ) {
 					n.style.removeProperty(o[i].p);
-					if (s[i][0] !== null)
-						n.style.setProperty(o[i].p, s[i][0], s[i][1])
+					if (s[i][0] !== null) n.style.setProperty(o[i].p, s[i][0], s[i][1]);
 				}
-				n = s = null
+				n = s = null;
 			}
 		}
 	})();
 	var _bind = function(evt, bind) {
-		if (bind === undefined)
-			bind = true;
-		if (evt.constructor !== Array)
-			evt = [evt];
+		if (bind === undefined)	bind = true;
+		if (evt.constructor !== Array) evt = [evt];
 		var method = bind ? 'addEventListener' : 'removeEventListener';
 		for (var i = 0, len = evt.length; i < len; i += 1)
-			document[method](evt[i], handlers[evt[i]], true)
+			document[method](evt[i], handlers[evt[i]], true);
 	};
 	var _unbind = function(evt) {
-		_bind(evt, false)
+		_bind(evt, false);
 	};
 	var cursor;
 	var needDetermineUserSelection;
@@ -88,17 +80,12 @@ window.SelectLikeABoss = window.SelectLikeABoss || (function() {
 	var needStopClick;
 	var resetVars = function() {
 		needDetermineUserSelection = needCreateStartSelection = true;
-		userSelecting = needStopClick = false
+		userSelecting = needStopClick = false;
 	};
 	var movable;
 	var s = document.getSelection();
-	function isLink(node){
-		if		(node.href)			return true;
-		else if (!node.parentNode)	return false;
-		else						return isLink( node.parentNode );
-	}
 	var mainMouseDownHandler = function(e) {
-		if (e.which !== 1 || !isLink(e.target)) return; // LMB on links only
+		if (e.which !== 1 || getCurrentAnchor(e.target) === null) return; // LMB on links only
 		
 		resetVars();
 		var x = e.clientX;
@@ -108,21 +95,17 @@ window.SelectLikeABoss = window.SelectLikeABoss || (function() {
 			if (range && s.getRangeAt(0).isPointInRange(range.startContainer, range.startOffset)) return;
 		}
 		_letUserSelect();
-		var t = e.target;
-		var n = getCurrentAnchor(t);
-		if (!n) {
-			n = t.nodeType !== 3 ? t : t.parentNode;
-		}
-		if (n.constructor === HTMLTextAreaElement || n.constructor === HTMLCanvasElement || n.textContent === '') return;
+		var n = getCurrentAnchor(e.target);
 		var rect = n.getBoundingClientRect();
 		movable = {n: n,x: Math.round(rect.left),y: Math.round(rect.top),c: 0};
-		cursor = {x: x,y: y};
+		cursor = {x: x, y: y};
 		_bind(['mousemove', 'mouseup', 'dragend', 'dragstart']);
 		_letUserSelect(n);
 	};
 	var D = 3;
 	var K = 0.8;
-	var handlers = {'mousemove': function(e) {
+	var handlers = {
+		mousemove: function(e) {
 			if (movable) {
 				if (movable.c++ < 12) {
 					var rect = movable.n.getBoundingClientRect();
@@ -130,11 +113,10 @@ window.SelectLikeABoss = window.SelectLikeABoss || (function() {
 						_unbind(['mousemove', 'mouseup', 'dragend', 'dragstart', 'click']);
 						_letUserSelect();
 						s.removeAllRanges();
-						return
+						return;
 					}
-				} else {
-					movable = null
 				}
+				else movable = null;
 			}
 			var x = e.clientX;
 			var y = e.clientY;
@@ -144,7 +126,7 @@ window.SelectLikeABoss = window.SelectLikeABoss || (function() {
 				var range = browser.getRangeFromPoint(x + correct, y);
 				if (range) {
 					s.addRange(range);
-					needCreateStartSelection = false
+					needCreateStartSelection = false;
 				}
 			}
 			if (needDetermineUserSelection) {
@@ -154,44 +136,31 @@ window.SelectLikeABoss = window.SelectLikeABoss || (function() {
 				if (vx > D || vy > D) {
 					needDetermineUserSelection = false;
 					needStopClick = true;
-					_bind('click')
+					_bind('click');
 				}
 			}
 			if (userSelecting) {
 				var range = browser.getRangeFromPoint(x, y);
-				if (range)
-					s.extend(range.startContainer, range.startOffset)
+				if (range) s.extend(range.startContainer, range.startOffset);
 			}
-		},'dragstart': function(e) {
+		},
+		dragstart: function(e) {
 			_unbind('dragstart');
-			if (userSelecting)
-				return stopEvent(e)
-		},'mouseup': function(e) {
+			if (userSelecting) return stopEvent(e);
+		},
+		mouseup: function(e) {
 			_unbind(['mousemove', 'mouseup', 'dragstart', 'dragend']);
-			if (!userSelecting && needStopClick)
-				needStopClick = false;
-			setTimeout(function() {
-				_unbind('click')
-			}, 111);
-			if (s.type !== 'Range')
-				_letUserSelect()
-		},'dragend': function(e) {
-			_unbind(['dragend', 'mousemove', 'mouseup'])
-		},'click': function(e) {
+			if (!userSelecting && needStopClick) needStopClick = false;
+			setTimeout(function() {	_unbind('click') }, 111);
+			if (s.type !== 'Range') _letUserSelect();
+		},
+		dragend: function(e) {
+			_unbind(['dragend', 'mousemove', 'mouseup']);
+		},
+		click: function(e) {
 			_unbind('click');
-			if (needStopClick)
-				return stopEvent(e)
+			if (needStopClick) return stopEvent(e);
 		}};
-	var enabled;
-	return {toggle: function(is) {
-			enabled = is !== undefined ? is : !enabled;
-			document[enabled ? 'addEventListener' : 'removeEventListener']('mousedown', mainMouseDownHandler, true);
-			if (!enabled)
-				_letUserSelect()
-		},on: function() {
-			this.toggle(true)
-		},off: function() {
-			this.toggle(false)
-		}}
-})(window);
-window.SelectLikeABoss.on();
+	
+	document.addEventListener('mousedown', mainMouseDownHandler, true);
+})();
