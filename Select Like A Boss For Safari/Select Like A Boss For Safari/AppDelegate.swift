5e635493f7f2d7cd544efd34bf2f1bf379736ead
Sergei Armodin <dolph-in@yandex.ru>
2021-11-15 21:39:39 +0300

    handle reopen on window close

--- Select Like A Boss For Safari/Select Like A Boss For Safari/AppDelegate.swift
+++ Select Like A Boss For Safari/Select Like A Boss For Safari/AppDelegate.swift
@@ -17,5 +17,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
 	}
+
+	func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+		if let windowController = NSStoryboard(name: "Main", bundle: nil).instantiateController(withIdentifier: "WindowController") as? WindowController {
+			windowController.window?.makeKeyAndOrderFront(nil)
+			return true
+		}
+		return false
+	}
 }
 
---
+++ Select Like A Boss For Safari/Select Like A Boss For Safari/WindowController.swift
@@ -0,0 +1,20 @@
+//
+//  WindowController.swift
+//  Select Like A Boss For Safari
+//
+//  Created by Sergei Armodin on 15.11.2021.
+//  Copyright © 2021 Sergei Armodin. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+
+}
